d3e07fdc8d3eb4e276be3039206b017ac2e5153d
Matheus Lessa Rodrigues <matheus@fastmail.com>
2018-08-14 20:26:48 -0300

    Create up.ps1

---
+++ up.ps1
@@ -0,0 +1,10 @@
+Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+choco install firefox
+choco install vscode
+choco install hg
+choco install git
+choco install discord
+choco install steam
+choco install spotify
+choco install reaper
+choco install itch
