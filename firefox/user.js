032b760078e7be74dd440ff5d1ac34c33f1cb2d9
Matheus Lessa Rodrigues <matheus@fastmail.com>
2018-08-28 15:35:00 -0300

    Update user.js

--- firefox/user.js
+++ firefox/user.js
@@ -7,30 +7,28 @@ user_pref("browser.urlbar.suggest.searches", false);
 user_pref("browser.search.update", false);
 
 // new tab
-user_pref("browser.startup.homepage", "about:blank");
-user_pref("browser.newtabpage.enabled", false);
+user_pref("browser.urlbar.placeholderName", "DuckDuckGo");
+user_pref("browser.startup.homepage", "data:text/html,<html style='background:black'><title>New Tab</title></html>");
+user_pref("browser.newtabpage.enabled", true);
 user_pref("browser.newtab.url", "about:blank");
 user_pref("browser.newtabpage.activity-stream.enabled", false);
 user_pref("browser.newtabpage.enhanced", false);
-user_pref("browser.newtab.preload", false);
+user_pref("browser.newtab.preload", true);
 user_pref("browser.newtabpage.directory.ping", "");
 user_pref("browser.newtabpage.directory.source", "data:text/plain,{}");
-user_pref("browser.urlbar.placeholderName", "DuckDuckGo");
 user_pref("browser.newtabpage.introShown", true);
 
-// color
-user_pref("reader.color_scheme", "dark");
-user_pref("browser.display.foreground_color", "#C0C0C0");
-user_pref("browser.display.background_color", "#000000");
-
 // ui
+user_pref("reader.color_scheme", "dark");
 user_pref("lightweightThemes.selectedThemeID", "firefox-compact-dark@mozilla.org");
 user_pref("devtools.theme", "dark");
+user_pref("browser.uidensity", 1);
 user_pref("browser.uiCustomization.state", '{"placements":{"widget-overflow-fixed-list":["languagetool-webextension_languagetool_org-browser-action","ublock0-button","downloads-button","sync-button","ublock0_raymondhill_net-browser-action","87677a2c52b84ad3a151a4a72f5bd3c4_jetpack-browser-action"],"PersonalToolbar":["personal-bookmarks"],"nav-bar":["back-button","forward-button","toggle-button--supportlastpasscom-lastpass-button","stop-reload-button","urlbar-container"],"toolbar-menubar":["menubar-items"],"TabsToolbar":["tabbrowser-tabs","alltabs-button"]},"seen":["pocket-button","developer-button","webide-button","87677a2c52b84ad3a151a4a72f5bd3c4_jetpack-browser-action","ublock0-button","languagetool-webextension_languagetool_org-browser-action","action-button--keybinderfailcl-keybinderoolbarutton","screenshots_mozilla_org-browser-action","jid1-niffy2ca8fy1tg_jetpack-browser-action","ublock0_raymondhill_net-browser-action","toggle-button--supportlastpasscom-lastpass-button"],"dirtyAreaCache":["PersonalToolbar","nav-bar","TabsToolbar","toolbar-menubar","PanelUI-contents","addon-bar","widget-overflow-fixed-list"],"currentVersion":14,"newElementCount":41}');
 user_pref("browser.uidensity", 1);
 user_pref("browser.download.forbid_open_with", true);
 user_pref("browser.download.autohideButton", false);
 user_pref("services.sync.declinedEngines", "passwords,bookmarks");
+user_pref("ui.osk.enabled", false);
 
 // disable pocket
 user_pref("browser.newtabpage.activity-stream.feeds.section.topstories", false);
