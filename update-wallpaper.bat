ea94bc90d57e4a35a86d5109a390205f4dc33c8f
Matheus Lessa Rodrigues <matheus@fastmail.com>
2018-10-29 19:01:00 -0300

    Update update-wallpaper.bat

--- update-wallpaper.bat
+++ update-wallpaper.bat
@@ -1,21 +1,13 @@
 @echo off
 
-REM schtasks /create /tn "start" /sc onstart /delay 0000:30 /rl highest /ru system /tr "powershell.exe -file <<The powershell script path>>
-REM put inside windows+r "shell:startup"
+rem put inside windows+r "shell:startup"
 
 set wallpaperLocation=%USERPROFILE%\Pictures\SpotlightWallpapers
 
 mkdir %wallpaperLocation%
 
-FOR /F "tokens=3* USEBACKQ" %%F IN (`reg query "HKCU\Software\Microsoft\Windows\CurrentVersion\Lock Screen\Creative" /v LandscapeAssetPath`) DO (
-SET spotlightLocation=%%F
-echo %%F
+for /F "tokens=3* USEBACKQ" %%F in (`reg query "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Lock Screen\Creative" /v LandscapeAssetPath`) do (
+	set spotlightFile=%%F
 )
 
-COPY /Y %spotlightLocation% %wallpaperLocation%\current_spotlight.jpg
-
-REM reg delete "HKEY_CURRENT_USER\Control Panel\Desktop" /v TranscodedImageCache /f
-REM reg add "HKEY_CURRENT_USER\Control Panel\Desktop" /v Wallpaper /t REG_SZ /d %wallpaperLocation%\current_spotlight.bmp /f
-REM reg add "HKEY_CURRENT_USER\Control Panel\Desktop" /v WallpaperStyle /t REG_SZ /d 10 /f
-
-REM RUNDLL32.EXE user32.dll,UpdatePerUserSystemParameters
+COPY /Y %spotlightFile% %wallpaperLocation%\current_spotlight.jpg
