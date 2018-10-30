68f99a7a219193663978bff47112b37ca91b0ebe
Matheus Lessa Rodrigues <matheus@fastmail.com>
2018-10-30 17:22:09 -0300

    Update update-wallpaper.bat

--- update-wallpaper.bat
+++ update-wallpaper.bat
@@ -1,13 +1,22 @@
 @echo off
-
 rem put inside windows+r "shell:startup"
 
 set wallpaperLocation=%USERPROFILE%\Pictures\SpotlightWallpapers
 
 mkdir %wallpaperLocation%
 
-for /F "tokens=3* USEBACKQ" %%F in (`reg query "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Lock Screen\Creative" /v LandscapeAssetPath`) do (
-	set spotlightFile=%%F
+for /F "tokens=3* usebackq" %%F in (`reg query "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Lock Screen\Creative" /v HotspotImageFolderPath`) do (
+	set spotlightFolder=%%F
 )
 
-COPY /Y %spotlightFile% %wallpaperLocation%\current_spotlight.jpg
+echo %spotlightFolder%
+
+for /R %spotlightFolder% %%F in (*) do (
+	for /F "tokens=1" %%D in ("%%~tF") do (
+		if %%D EQU %date% (
+			for /F "usebackq" %%R in (`powershell -command "$i=New-Object -ComObject Wia.ImageFile; $i.LoadFile('%%F'); if($i.Width -gt $i.Height){$true}"`) do (
+				copy /Y %%F %wallpaperLocation%\*.jpg
+			)
+		)
+	)
+)
