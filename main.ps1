18069abd94541914faf05078bcd1f9fd706d287a
Matheus Lessa Rodrigues <matheus@fastmail.com>
2018-11-20 19:45:21 -0200

    Update main.ps1

--- main.ps1
+++ main.ps1
@@ -2,18 +2,26 @@
 
 iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
+// Main
 choco install keepass -y
 choco install firefox -y
+choco install imageglass -y
+choco install sharex -y
+choco install windirstat -y
+choco install spotify -y
+choco install logitechgaming -y
+
+// Communication
+choco install telegram -y
+choco install discord -y
+
+// Dev
+choco install git --params "/GitOnlyOnPath /NoShellIntegration" -y
+choco install hg -y
+choco install putty -y
 choco install vscode --params "/NoContextMenuFile /NoContextMenuFolders" -y
 choco install dotnetcore-sdk -y
-choco install putty -y
-choco install hg -y
-choco install git --params "/GitOnlyOnPath /NoShellIntegration" -y
-choco install discord -y
+
+// Games
 choco install steam -y
-choco install spotify -y
 choco install itch -y
-choco install windirstat -y
-choco install sharex -y
-choco install logitechgaming -y
-choco install telegram -y
