92963d33e306bdf1b2f425953f78441254b46f76
Matheus Lessa Rodrigues <matheus@fastmail.com>
2019-02-13 21:29:07 +0000

    Update Microsoft.PowerShell_profile.ps1

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -51,3 +51,102 @@ Function Pick {
 		write-host $obj
 	}
 }
+
+Function Get-Todo-File {
+	Process {
+		$file = join-path -Path $env:UserProfile -ChildPath "todo.txt"
+		if (!(Test-Path $file)) {
+			New-Item -path $env:UserProfile -name "todo.txt" -type "file"
+		}
+
+		return $file
+	}
+}
+
+Function Todo-List {
+	[cmdletbinding()]
+	[Alias("tl")]
+	Param ()
+	Process {
+		$file = Get-Todo-File
+		$lines = get-content $file
+
+		for ($i=0; $i -lt $lines.Length; $i++) {
+			write-host "[", $i, "]", $lines[$i]
+		}
+	}
+}
+
+Function Todo-Add {
+	[cmdletbinding()]
+	[Alias("ta")]
+	Param (
+		[Parameter(ValueFromRemainingArguments)]
+		[string[]]$Task
+	)
+	Process {
+		$file = Get-Todo-File
+		$line = $Task -join ' '
+		add-content -path $file -value $line
+	}
+}
+
+Function Todo-RemoveAt {
+	[cmdletbinding()]
+	[Alias("tr")]
+	Param (
+		[Parameter(ValueFromRemainingArguments)]
+		[int[]]$Indexes
+	)
+	Process {
+		$file = Get-Todo-File
+		$content = get-content $file
+		[System.Collections.ArrayList]$lines = $content
+
+		$Indexes = $Indexes | sort -desc
+		foreach($index in $Indexes) {
+			$lines.RemoveAt($index)
+		}
+
+		set-content -path $file -value $lines
+	}
+}
+
+Function Todo-RemoveWhere {
+	[cmdletbinding()]
+	[Alias("tw")]
+	Param (
+		[Alias("a")]
+		[switch]$All,
+		[Parameter(ValueFromRemainingArguments)]
+		[string[]]$Text
+	)
+	Process {
+		$file = Get-Todo-File
+		$content = get-content $file
+
+		$Text = $Text -join ' '
+		$Text = "(\s+|^)" + $Text + "(\s+|$)"
+
+		$filtered = @($content | where { $_ -notmatch $Text })
+
+		if (($filtered.Length -eq ($content.Length - 1)) -or $All) {
+			set-content -path $file -value $content
+		} elseif ($filtered.Length -lt ($content.Length - 1)) {
+			$matches = $content | where { $_ -match $Text }
+
+			write-host "More than one match:"
+			write-host -Object $matches -Separator `n
+		}
+	}
+}
+
+Function Todo-Delete {
+	[cmdletbinding()]
+	[Alias("tdel")]
+	Param ()
+	Process {
+		$file = Get-Todo-File
+		del $file
+	}
+}
