df30803c27756f7f021a1c7a0872988236283260
Matheus Lessa Rodrigues <matheus@fastmail.com>
2019-02-13 19:27:03 +0000

    Update Microsoft.PowerShell_profile.ps1

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -26,3 +26,28 @@ Function Find-String {
 		Get-ChildItem -Path . -Filter $Filter -Recurse | Select-String -Pattern $Patterns | Select-Object -Unique Path | %{$_.Path}
 	}
 }
+
+Function Pick {
+	[cmdletbinding()]
+	[Alias("p")]
+	Param (
+		[parameter(ValueFromPipeline)]
+		[Object]$Obj
+	)
+	Begin {
+		$objs = New-Object System.Collections.Generic.List[System.Object]
+	}
+	Process {
+		$objs.Add($Obj)
+	}
+	End {
+		for ($i=0; $i -lt $objs.Count; $i++) {
+			write-host "[", $i, "]", $objs[$i]
+		}
+
+		$index = read-host "Pick"
+		$obj = $objs[$index]
+		set-clipboard -Value $obj
+		write-host $obj
+	}
+}
