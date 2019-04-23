af3f8c5d8059cfe301c307968dcb35ec5b1ab078
Matheus Lessa Rodrigues <matheus@fastmail.com>
2019-04-23 15:54:10 +0000

    Update main.ps1

--- main.ps1
+++ main.ps1
@@ -1,10 +1,10 @@
 iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
 // Main
+choco install fzf -y
 choco install firefox -y
 choco install sharex -y
 choco install windirstat -y
-choco install logitechgaming -y
 choco install spotify -y
 
 // Communication
@@ -16,16 +16,10 @@ choco install hg -y
 choco install putty -y
 choco install vscode --params "/NoContextMenuFile /NoContextMenuFolders" -y
 choco install dotnetcore-sdk -y
-//choco install netfx-4.7.1-devpack -y
 
 // Games
 choco install steam -y
 choco install itch -y
-
-// Archive
-//choco install keepass -y
-//choco install brave -y
-//choco install imageglass -y
 
 // PowerShell Profile
 ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/matheuslessarodrigues/up/master/Microsoft.PowerShell_profile.ps1')) > $Profile
