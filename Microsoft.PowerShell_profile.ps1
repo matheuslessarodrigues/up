4f2b7ebee7f52b5a3c79450515d28465bd6d28da
Matheus Lessa Rodrigues <matheus@fastmail.com>
2019-04-29 17:30:54 +0000

    Update Microsoft.PowerShell_profile.ps1

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -256,5 +256,5 @@ Function Todo-Delete {
 # Chocolatey profile
 $ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
 if (Test-Path($ChocolateyProfile)) {
-  Import-Module "$ChocolateyProfile"
+	Import-Module "$ChocolateyProfile"
 }
