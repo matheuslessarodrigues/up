0e98cf100da5f223ce8d0377c4feb56047032cbc
Matheus Lessa Rodrigues <matheus@fastmail.com>
2019-05-27 00:29:01 +0000

    Update main.ps1

--- main.ps1
+++ main.ps1
@@ -1,6 +1,4 @@
-$url = "https://matheuslessarodrigues.github.io/up"
-
-iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+iex (curl.exe 'https://chocolatey.org/install.ps1')
 
 // Main
 choco install fzf -y
@@ -23,13 +21,4 @@ choco install dotnetcore-sdk -y
 choco install steam -y
 choco install itch -y
 
-// PowerShell Profile
-((new-object System.Net.WebClient).DownloadString("$url/Microsoft.PowerShell_profile.ps1")) > $Profile
-
-// Firefox Profile
-$firefoxProfile = ((new-object System.Net.WebClient).DownloadString("$url/firefox/user.js"))
-get-childitem -path "$env:APPDATA/Mozilla/Firefox/Profiles" -Directory | %{$path = join-path $_.FullName "user.js"; set-content -path $path -value $firefoxProfile;}
-
-// Update Wallpaper
-$updateWallpaper = ((new-object System.Net.WebClient).DownloadString("$url/update-wallpaper.bat"))
-set-content -path "$env:appdata\Microsoft\Windows\Start Menu\Programs\Startup" -value $updateWallpaper
+iex (curl.exe 'https://matheuslessarodrigues.github.io/up/profiles.ps1')
