5c8971086018833d81c1f8ddccff775c7943f28c
Matheus Lessa Rodrigues <matheus@fastmail.com>
2019-05-27 01:21:40 +0000

    Update profiles.ps1

--- profiles.ps1
+++ profiles.ps1
@@ -8,5 +8,5 @@ $firefoxProfile = (curl.exe -s "$url/firefox/user.js" | out-string)
 get-childitem -path "$env:APPDATA/Mozilla/Firefox/Profiles" -Directory | %{$path = join-path $_.FullName "user.js"; set-content -path $path -value $firefoxProfile;}
 
 # Update Wallpaper
-$updateWallpaper = (curl.exe -s "$url/update-wallpaper.bat" | out-string)
-set-content -path "$env:appdata\Microsoft\Windows\Start Menu\Programs\Startup" -value $updateWallpaper
+#$updateWallpaper = (curl.exe -s "$url/update-wallpaper.bat" | out-string)
+#set-content -path "$env:appdata\Microsoft\Windows\Start Menu\Programs\Startup" -value $updateWallpaper
