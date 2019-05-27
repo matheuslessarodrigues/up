a3dbec629ad92c6162d4741d9009024f0fc5eb24
Matheus Lessa Rodrigues <matheus@fastmail.com>
2019-05-27 00:54:27 +0000

    Update main.ps1

--- main.ps1
+++ main.ps1
@@ -1,24 +1,25 @@
-iex (curl.exe 'https://chocolatey.org/install.ps1')
+iex (curl.exe -s 'https://chocolatey.org/install.ps1' | out-string)
 
-// Main
-//choco install fzf -y
+# Main
+#choco install fzf -y
 choco install firefox -y
 choco install sharex -y
 choco install windirstat -y
 choco install spotify -y
 
-// Communication
+# Communication
 choco install discord -y
 
-// Dev
+# Dev
 choco install git --params "/GitOnlyOnPath /NoShellIntegration" -y
 choco install hg -y
 choco install putty -y
 choco install vscode --params "/NoContextMenuFile /NoContextMenuFolders" -y
 choco install dotnetcore-sdk -y
 
-// Games
+# Games
 choco install steam -y
 choco install itch -y
 
-iex (curl.exe 'https://matheuslessarodrigues.github.io/up/profiles.ps1')
+# Update Profiles
+iex (curl.exe -s 'https://matheuslessarodrigues.github.io/up/profiles.ps1' | out-string)
