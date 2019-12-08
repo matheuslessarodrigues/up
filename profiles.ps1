957e92dae22586b8eef615fdb05c1877f12f5f0e
Matheus Lessa Rodrigues <matheus@fastmail.com>
2019-12-08 00:40:59 +0000

    Update profiles.ps1

--- profiles.ps1
+++ profiles.ps1
@@ -11,3 +11,4 @@ get-childitem -path "$env:APPDATA/Mozilla/Firefox/Profiles" -Directory | %{$path
 # Update Wallpaper
 $updateWallpaper = (curl.exe -s "$url/update-wallpaper.bat" | out-string)
 set-content -path "$env:appdata\Microsoft\Windows\Start Menu\Programs\Startup\update-wallpaper.bat" -value $updateWallpaper
+."$env:appdata\Microsoft\Windows\Start Menu\Programs\Startup\update-wallpaper.bat"
