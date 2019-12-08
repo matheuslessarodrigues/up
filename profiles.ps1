9ec1cb795ee0c3d6272a356da0c25296aecea5bd
Matheus Lessa Rodrigues <matheus@fastmail.com>
2019-12-08 00:34:07 +0000

    Update profiles.ps1

--- profiles.ps1
+++ profiles.ps1
@@ -1,13 +1,13 @@
 $url = "https://matheuslessarodrigues.github.io/up"
 
 # PowerShell Profile
-new-item -path $profile -force
-(curl.exe -s "$url/Microsoft.PowerShell_profile.ps1" | out-string) > $Profile
+$powershellProfile = (curl.exe -s "$url/Microsoft.PowerShell_profile.ps1" | out-string)
+set-content -path $profile -value $powershellProfile
 
 # Firefox Profile
 $firefoxProfile = (curl.exe -s "$url/firefox/user.js" | out-string)
 get-childitem -path "$env:APPDATA/Mozilla/Firefox/Profiles" -Directory | %{$path = join-path $_.FullName "user.js"; set-content -path $path -value $firefoxProfile;}
 
 # Update Wallpaper
-#$updateWallpaper = (curl.exe -s "$url/update-wallpaper.bat" | out-string)
-#set-content -path "$env:appdata\Microsoft\Windows\Start Menu\Programs\Startup" -value $updateWallpaper
+$updateWallpaper = (curl.exe -s "$url/update-wallpaper.bat" | out-string)
+set-content -path "$env:appdata\Microsoft\Windows\Start Menu\Programs\Startup\UpdateWallpaper.bat" -value $updateWallpaper
