c31854649257641501a8aed2ee15725ecad3f27c
Matheus Lessa Rodrigues <matheus@fastmail.com>
2019-12-09 02:01:21 +0000

    Create update-wallpaper.ps1

---
+++ profiles/update-wallpaper.ps1
@@ -0,0 +1,15 @@
+$wallpaperLocation="$home\Pictures\SpotlightWallpapers"
+if(test-path $wallpaperLocation) {
+	remove-item -recurse -force $wallpaperLocation
+}
+mkdir $wallpaperLocation
+
+$spotlightFolder="$env:LOCALAPPDATA\Packages\Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy\LocalState\Assets"
+
+get-childitem $spotlightFolder | foreach-object {
+	$img=New-Object -ComObject Wia.ImageFile
+	$img.LoadFile($_)
+	if(($img.Width -gt $img.Height) -and ($img.Height -gt 720)) {
+		copy-item $_ -destination "$wallpaperLocation\$($_.BaseName).jpg"
+	}
+}
