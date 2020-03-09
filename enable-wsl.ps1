2b384e3918ae0d0ea229bfdde5045dab46ab6849
Matheus Lessa Rodrigues <matheus@fastmail.com>
2020-03-09 18:28:43 +0000

    Update enable-wsl.ps1

--- enable-wsl.ps1
+++ enable-wsl.ps1
@@ -1,1 +1,6 @@
-Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux
+dism.exe /online /enable-feature /featurename:Microsoft-Windows-Subsystem-Linux /all /norestart
+dism.exe /online /enable-feature /featurename:VirtualMachinePlatform /all /norestart
+
+# restart
+
+scoop install archwsl
