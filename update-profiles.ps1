7e901775db8c1ad0a5f79c066eab62492bf932b5
Matheus Lessa Rodrigues <matheus@fastmail.com>
2020-03-09 15:14:38 +0000

    Update update-profiles.ps1

--- update-profiles.ps1
+++ update-profiles.ps1
@@ -1,6 +1,6 @@
 function download($uri, $targetPath) {
-  write-host "downloading $uri..."
-  curl.exe "https://matheuslessarodrigues.github.io/up/profiles/$uri" -s -o $targetPath --create-dirs  
+  write-host "downloading $uri to $targetPath.."
+  curl.exe "https://matheuslessarodrigues.github.io/up/profiles/$uri" -s -o $targetPath --create-dirs
 }
 
 download("powershell.ps1", $profile)
@@ -11,3 +11,6 @@ download("mercurial.ini", "$home\mercurial.ini")
 download("update-wallpaper.ps1", "$home\update-wallpaper.ps1")
 
 get-childitem -path "$env:APPDATA/Mozilla/Firefox/Profiles" -Directory | %{download("firefox.js", (join-path $_.FullName "user.js"))}
+
+write-host ""
+write-host "all profiles updated!"
