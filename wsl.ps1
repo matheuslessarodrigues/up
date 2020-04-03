1a42ebd4cdaadb3e784dd8adfd046e7ba1f042df
Matheus Lessa Rodrigues <matheus@fastmail.com>
2020-04-03 12:22:14 +0000

    Update wsl.ps1

--- wsl.ps1
+++ wsl.ps1
@@ -17,10 +17,10 @@ wsl sudo pacman -Sy openssh --noconfirm
 wsl sudo pacman -Sy git --noconfirm
 wsl sudo pacman -Sy mercurial --noconfirm
 
-wsl sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-
 wsl sudo pacman -Sy fzf --noconfirm
 wsl sudo pacman -Sy ripgrep --noconfirm
 
 wsl sudo pacman -Sy kakoune --noconfirm
 wsl sudo pacman -Sy kak-lsp --noconfirm
+
+wsl sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
