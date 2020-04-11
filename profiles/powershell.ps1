70f6c4ce746f734381002142da076d61a4fb22c5
Matheus Lessa Rodrigues <matheus@fastmail.com>
2020-04-11 23:14:52 +0000

    Update powershell.ps1

--- profiles/powershell.ps1
+++ profiles/powershell.ps1
@@ -6,13 +6,8 @@ $env:BAT_PAGER="less -FR --no-init"
 $env:FZF_DEFAULT_COMMAND='fd --type f'
 $env:DOTNET_CLI_TELEMETRY_OPTOUT=$true
 
-Set-Alias -name vim -value nvim -force
-Set-Alias -name which -value where.exe -force
-Set-Alias -name uniq -value get-unique -force
-
-Remove-Alias -name cd -force
-Remove-Alias -name ls -force
-Remove-Alias -name cat -force
+get-alias | remove-alias -force
+set-alias -name vim -value nvim -force
 
 function clip {
 	param([parameter(position=0,mandatory=$true,ValueFromPipeline=$true)]$text)
@@ -29,27 +24,6 @@ function clip {
 			$data.ToString().TrimEnd([Environment]::NewLine) + [Convert]::ToChar(0) | clip.exe
 		}
 	}
-}
-
-function ls {
-	param(
-		[parameter(position=0,mandatory=$false,ValueFromPipeline=$true)]$location,
-		[switch]$hidden
-	)
-	$saved_pwd = pwd
-	if($location) {
-		set-location $location 2>$null
-		if(-not $?) {
-			write-error $error[0]
-			return
-		}
-	}
-	if($hidden -eq $true) {
-		fd -d 1 --hidden
-	} else {
-		fd -d 1
-	}
-	set-location $saved_pwd
 }
 
 function cd {
