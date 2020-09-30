25fbc7d3db15bf8e2661ed3e654025102a37b7f8
Matheus Lessa Rodrigues <matheus@fastmail.com>
2020-09-30 13:35:59 +0000

    Update pepper.lua

--- profiles/pepper.lua
+++ profiles/pepper.lua
@@ -15,3 +15,61 @@ function fzf()
 	fzf_windows_terminal()
 end
 keymap.normal("<c-o>", ":fzf()<enter>")
+
+-- c#
+local keywords = {"abstract", "as", "base", "break", "case", "catch", "checked", "class", "const", "continue", "default", "delegate", "do", "else", "enum", "event", "explicit", "extern", "finally", "fixed", "for", "foreach", "goto", "if", "implicit", "in", "interface", "internal", "is", "lock", "namespace", "new", "operator", "out", "override", "params", "private", "protected", "public", "readonly", "ref", "return", "sealed", "sizeof", "stackalloc", "static", "struct", "switch", "throw", "try", "typeof", "unchecked", "unsafe", "using", "virtual", "volatile", "while", "add", "alias", "ascending", "async", "await", "by", "descending", "dynamic", "equals", "from", "get", "global", "group", "into", "join", "let", "nameof", "notnull", "on", "orderby", "partial", "remove", "select", "set", "unmanaged", "value", "var", "when", "where", "yield"}
+for i,k in ipairs(keywords) do
+	syntax.rule("cs", "keyword", k)
+end
+
+local symbols = {"%(", "%)", "%[", "%]", "%{", "%}", ":", ";", ",", "=", "<", ">", "+", "-", "/", "*", "%%", "%.", "%!", "?", "&", "|", "@"}
+for i,k in ipairs(symbols) do
+	syntax.rule("cs", "symbol", k)
+end
+
+local types = {"bool", "byte", "char", "decimal", "double", "float", "int", "long", "object", "sbyte", "short", "string", "uint", "ulong", "ushort", "void"}
+for i,k in ipairs(types) do
+	syntax.rule("cs", "type", k)
+end
+syntax.rule("cs", "type", "%u{%w}");
+
+syntax.rule("cs", "comment", "//{.}");
+syntax.rule("cs", "comment", "/*{!(*/).$}");
+
+for i,k in ipairs({"null", "this", "true", "false"}) do
+	syntax.rule("cs", "literal", k);
+end
+
+syntax.rule("literal", "%d{%w%._}");
+syntax.rule("string", "'{(\\')!'.}");
+syntax.rule("string", "\"{(\\\")!\".}");
+
+syntax.rule("cs", "text", "%a{%w_}")
+syntax.rule("cs", "text", "_{%w_}")
+
+-- javascript
+local keywords = {"break", "export", "super", "case", "extends", "switch", "catch", "finally", "class", "for", "throw", "const", "function", "try", "continue", "if", "typeof", "debugger", "import", "var", "default", "in", "void", "delete", "instanceof", "while", "do", "new", "with", "else", "return", "yield", "enum", "implements", "package", "public", "interface", "private", "static", "let", "protected", "yield", "await", "abstract", "float", "synchronized", "boolean", "goto", "throws", "byte", "int", "transient", "char", "long", "volatile", "double", "native", "final", "short", "arguments", "get", "set"}
+for i,k in ipairs(keywords) do
+	syntax.rule("js", "keyword", k)
+end
+
+local symbols = {"%(", "%)", "%[", "%]", "%{", "%}", ":", ";", ",", "=", "<", ">", "+", "-", "/", "*", "%%", "%.", "%!", "?", "&", "|", "@"}
+for i,k in ipairs(symbols) do
+	syntax.rule("js", "symbol", k)
+end
+
+syntax.rule("js", "type", "%u{%w}");
+
+syntax.rule("js", "comment", "//{.}");
+syntax.rule("js", "comment", "/*{!(*/).$}");
+
+for i,k in ipairs({"null", "undefined",  "true", "false"}) do
+	syntax.rule("js", "literal", k);
+end
+
+syntax.rule("js", "literal", "%d{%w%._}");
+syntax.rule("js", "string", "'{(\\')!'.}");
+syntax.rule("js", "string", "\"{(\\\")!\".}");
+
+syntax.rule("js", "text", "%a{%w_}")
+syntax.rule("js", "text", "_{%w_}")
