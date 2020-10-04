57991f34316110df748f32086d4c208a69cf8197
Matheus Lessa Rodrigues <matheus@fastmail.com>
2020-10-04 15:30:40 +0000

    Update config-wsl.ps1

--- config-wsl.ps1
+++ config-wsl.ps1
@@ -1,9 +1,8 @@
 scoop install archwsl
-arch
 
 wsl sed -i '85s/#//' /etc/sudoers
 wsl useradd -m -G wheel matheus
-arch config --default-user matheus
+arch.exe config --default-user matheus
 wsl sudo passwd matheus
 
 wsl sudo pacman-key --init
@@ -11,17 +10,22 @@ wsl sudo pacman-key --populate archlinux
 wsl sudo pacman -Syyu --noconfirm
 
 wsl sudo pacman -Sy zsh --noconfirm
-wsl sudo pacman -Sy tmux --noconfirm
 
 wsl sudo pacman -Sy openssh --noconfirm
 wsl sudo pacman -Sy git --noconfirm
 wsl sudo pacman -Sy mercurial --noconfirm
 
 wsl sudo pacman -Sy fzf --noconfirm
+wsl sudo pacman -Sy fd --noconfirm
 wsl sudo pacman -Sy ripgrep --noconfirm
 
+wsl sudo pacman -Sy neovim --noconfirm
 wsl sudo pacman -Sy kakoune --noconfirm
 wsl sudo pacman -Sy kak-lsp --noconfirm
+
+wsl sudo pacman -Sy rustup --noconfirm
+wsl rustup toolchain install stable
+wsl cargo install verco
 
 wsl sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 wsl sudo chsh -s /bin/zsh matheus
