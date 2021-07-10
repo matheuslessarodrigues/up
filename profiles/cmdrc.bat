39f0a1c6350ab4f48d4e7931c25e74c214bc9fbc
Matheus Lessa Rodrigues <1696072+vamolessa@users.noreply.github.com>
2021-07-10 22:41:26 +0000

    Update cmdrc.bat

--- profiles/cmdrc.bat
+++ profiles/cmdrc.bat
@@ -6,7 +6,7 @@ prompt $E[30;43m$P$E[00m$S$E]9;9;$P$E\
 
 doskey /exename=cmd.exe clip=copycat $*
 doskey /exename=cmd.exe fd=fd --path-separator / $*
-doskey /exename=cmd.exe rg=rf --no-ignore-global --path-separator / $*
+doskey /exename=cmd.exe rg=rg --no-ignore-global --path-separator / $*
 doskey /exename=cmd.exe pp=pepper --config "%HOME%/pepper-config/init.pp" --try-config project.pp $*
 
 doskey /exename=cmd.exe ls=dir $*
