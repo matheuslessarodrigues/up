e1620e07702e29ac8749c68f83d256eed7fb83fb
Matheus Lessa Rodrigues <1696072+vamolessa@users.noreply.github.com>
2021-07-10 19:39:55 +0000

    Update cmdrc.bat

--- profiles/cmdrc.bat
+++ profiles/cmdrc.bat
@@ -1,9 +1,17 @@
 @echo off
 
-prompt $E[30;43m$P$E[00m$E]9;9;$P$E\$S
+prompt $E[30;43m$P$E[00m$S$E]9;9;$P$E\
 
 doskey /exename=cmd.exe clip=copycat $*
 doskey /exename=cmd.exe fd=fd --path-separator / $*
 doskey /exename=cmd.exe rg=rf --no-ignore-global --path-separator / $*
 doskey /exename=cmd.exe pp=pepper --config "%HOMEDRIVE%%HOMEPATH%/pepper-config/init.pp" --try-config project.pp $*
-doskey /exename=cmd.exe ls=ls -AphX --color=auto $*
+
+doskey /exename=cmd.exe ls=dir $*
+doskey /exename=cmd.exe rm=del $*
+doskey /exename=cmd.exe mv=move $*
+doskey /exename=cmd.exe cp=copy $*
+doskey /exename=cmd.exe cat=type $*
+doskey /exename=cmd.exe which=where $*
+doskey /exename=cmd.exe ps=tasklist $*
+doskey /exename=cmd.exe kill=taskkill $*
