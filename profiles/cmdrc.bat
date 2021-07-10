2fe2fa318cf5550bab86a1436e6aee395f593612
Matheus Lessa Rodrigues <1696072+vamolessa@users.noreply.github.com>
2021-07-10 17:12:17 +0000

    Create cmdrc.bat

---
+++ profiles/cmdrc.bat
@@ -0,0 +1,11 @@
+@echo off
+
+prompt $E[30;43m$P$E[00m$E]9;9;$P$E\$S
+
+doskey /exename=cmd.exe clip=copycat $*
+doskey /exename=cmd.exe fd=fd --path-separator / $*
+doskey /exename=cmd.exe rg=rf --no-ignore-global --path-separator / $*
+doskey /exename=cmd.exe pp=pepper --config "%HOMEDRIVE%%HOMEPATH%/pepper-config/init.pp" --try-config project.pp $*
+doskey /exename=cmd.exe ls=dir $*
+doskey /exename=cmd.exe cat=type $*
+doskey /exename=cmd.exe which=where $*
